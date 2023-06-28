727cf3d6d7674fc03e11122e65b39aff8d621db3
juanilato <105721274+juanilato@users.noreply.github.com>
2023-06-28 14:33:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 from tkinter import *
 from tkinter import ttk
 from functools import partial
+import cmath
 
 
-class Calculadora(object):
-    __ventana=None
-    __operador=None
-    __panel=None
-    __operadorAux=None
-    __primerOperando=None
-    __segundoOperando=None
-    
+class Calculadora:
     def __init__(self):
         self.__ventana = Tk()
-        self.__ventana.title('Tk-Calculadora')
+        self.__ventana.title('Calculadora de Números Complejos')
         mainframe = ttk.Frame(self.__ventana, padding="3 10 3 10")
+        self.__ventana.resizable(0, 0)
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         mainframe.columnconfigure(0, weight=1)
         mainframe.rowconfigure(0, weight=1)
@@ -23,10 +18,15 @@ class Calculadora(object):
         self.__panel = StringVar()
         self.__operador = StringVar()
         self.__operadorAux = None
+        self.__primerOperando = None
+        self.__segundoOperando = None
+        self.__contador = 0
+        opts = { 'ipadx': 18, 'ipady': 60 , 'sticky': 's' }
         operatorEntry = ttk.Entry(mainframe, width=10, textvariable=self.__operador, justify='center', state='disabled')
         operatorEntry.grid(column=1, row=1, columnspan=1, sticky=(W, E))
         panelEntry = ttk.Entry(mainframe, width=20, textvariable=self.__panel, justify='right', state='disabled')
         panelEntry.grid(column=2, row=1, columnspan=2, sticky=(W, E))
+        
         ttk.Button(mainframe, text='1', command=partial(self.ponerNUMERO, '1')).grid(column=1, row=3, sticky=W)
         ttk.Button(mainframe, text='2', command=partial(self.ponerNUMERO, '2')).grid(column=2, row=3, sticky=W)
         ttk.Button(mainframe, text='3', command=partial(self.ponerNUMERO, '3')).grid(column=3, row=3, sticky=W)
@@ -37,15 +37,22 @@ class Calculadora(object):
         ttk.Button(mainframe, text='8', command=partial(self.ponerNUMERO, '8')).grid(column=2, row=5, sticky=W)
         ttk.Button(mainframe, text='9', command=partial(self.ponerNUMERO, '9')).grid(column=3, row=5, sticky=W)
         ttk.Button(mainframe, text='0', command=partial(self.ponerNUMERO, '0')).grid(column=1, row=6, sticky=W)
+        ttk.Button(mainframe, text='i', command=partial(self.ponerNUMERO, 'i')).grid(column=4, row=7)
         ttk.Button(mainframe, text='+', command=partial(self.ponerOPERADOR, '+')).grid(column=2, row=6, sticky=W)
         ttk.Button(mainframe, text='-', command=partial(self.ponerOPERADOR, '-')).grid(column=3, row=6, sticky=W)
         ttk.Button(mainframe, text='*', command=partial(self.ponerOPERADOR, '*')).grid(column=1, row=7, sticky=W)
         ttk.Button(mainframe, text='/', command=partial(self.ponerOPERADOR, '/')).grid(column=2, row=7, sticky=W)
         ttk.Button(mainframe, text='=', command=partial(self.ponerOPERADOR, '=')).grid(column=3, row=7, sticky=W)
-        self.__panel.set('0')
+        ttk.Button(mainframe, text='+', command=partial(self.ponerNUMERO, '+')).grid(column=1, row=8, sticky=W)
+        ttk.Button(mainframe, text='-', command=partial(self.ponerNUMERO, '-')).grid(column=2, row=8, sticky=W)
+        ttk.Button(mainframe, text='*', command=partial(self.ponerNUMERO, '*')).grid(column=3, row=8, sticky=W)
+        ttk.Button(mainframe, text='/', command=partial(self.ponerNUMERO, '/')).grid(column=4, row=8, sticky=W)
+        ttk.Button(mainframe, text='C', command=self.borrarPanel).grid(column=4, row=4, sticky=W)
+        
+        self.__panel.set('')
         panelEntry.focus()
         self.__ventana.mainloop()
-    
+
     def ponerNUMERO(self, numero):
         if self.__operadorAux == None:
             valor = self.__panel.get()
@@ -53,12 +60,12 @@ class Calculadora(object):
         else:
             self.__operadorAux = None
             valor = self.__panel.get()
-            self.__primerOperando = Imaginario(int(valor), 0)
+            self.__primerOperando = self.convertir(str(valor))
             self.__panel.set(numero)
-    
+
     def borrarPanel(self):
-        self.__panel.set('0')
-    
+        self.__panel.set('')
+
     def resolverOperacion(self, operando1, operacion, operando2):
         resultado = 0
         if operacion == '+':
@@ -70,11 +77,11 @@ class Calculadora(object):
         elif operacion == '/':
             resultado = operando1 / operando2
         self.__panel.set(str(resultado))
-    
+
     def ponerOPERADOR(self, op):
         if op == '=':
             operacion = self.__operador.get()
-            self.__segundoOperando = Imaginario(int(self.__panel.get()), 0)
+            self.__segundoOperando = self.convertir(str(self.__panel.get()))
             self.resolverOperacion(self.__primerOperando, operacion, self.__segundoOperando)
             self.__operador.set('')
             self.__operadorAux = None
@@ -84,40 +91,68 @@ class Calculadora(object):
                 self.__operadorAux = op
             else:
                 operacion = self.__operador.get()
-                self.__segundoOperando = Imaginario(int(self.__panel.get()), 0)
+                self.__segundoOperando = self.convertir(str(self.__panel.get()))
                 self.resolverOperacion(self.__primerOperando, operacion, self.__segundoOperando)
                 self.__operador.set(op)
                 self.__operadorAux = op
+
+    def convertir(self, cadena):
+        if cadena == '+':
+            return '+'
+        elif cadena == '-':
+            return '-'
+        elif cadena == '*':
+            return '*'
+        elif cadena == '/':
+            return '/'
+        elif 'i' in cadena:
+            partes = cadena.split('+')
+            if len(partes) > 1:
+                real = float(partes[0])
+                imag = float(partes[1][:-1])
+                return Imaginario(real, imag)
+            elif len(partes) == 1:
+                partes = cadena.split('-')
+                real = float(partes[0])
+                imag = float(partes[1][:-1]) * -1
+                return Imaginario(real, imag)
+        else:
+            return float(cadena)
 
 class Imaginario:
     def __init__(self, real, imaginario):
         self.real = real
         self.imaginario = imaginario
     
-    def __str__(self):
-        return f"{self.real}+{self.imaginario}i"
-    
-    def __add__(self, otro):
-        real = self.real + otro.real
-        imaginario = self.imaginario + otro.imaginario
-        return Imaginario(real, imaginario)
-    
-    def __sub__(self, otro):
-        real = self.real - otro.real
-        imaginario = self.imaginario - otro.imaginario
-        return Imaginario(real, imaginario)
-    
-    def __mul__(self, otro):
-        real = self.real * otro.real - self.imaginario * otro.imaginario
-        imaginario = self.real * otro.imaginario + self.imaginario * otro.real
-        return Imaginario(real, imaginario)
-    
-    def __truediv__(self, otro):
-        divisor = otro.real**2 + otro.imaginario**2
-        real = (self.real * otro.real + self.imaginario * otro.imaginario) / divisor
-        imaginario = (self.imaginario * otro.real - self.real * otro.imaginario) / divisor
+    def getImaginario(self):
+        return self.imaginario
+    def getReal(self):
+        return self.real
+
+    def __add__(self, other):
+        real = self.real + other.real
+        imaginario = self.imaginario + other.imaginario
         return Imaginario(real, imaginario)
 
+    def __sub__(self, other):
+        real = self.real - other.real
+        imaginario = self.imaginario - other.imaginario
+        return Imaginario(real, imaginario)
+
+    def __mul__(self, other):
+        real = (self.real * other.real) - (self.imaginario * other.imaginario)
+        imaginario = (self.real * other.imaginario) + (self.imaginario * other.real)
+        return Imaginario(real, imaginario)
+
+    def __truediv__(self, other):
+        divisor = (other.real ** 2) + (other.imaginario ** 2)
+        real = ((self.real * other.real) + (self.imaginario * other.imaginario)) / divisor
+        imaginario = ((self.imaginario * other.real) - (self.real * other.imaginario)) / divisor
+        return Imaginario(real, imaginario)
+                        
+
+    def __str__(self):
+            return f"{str(self.real)} + {str(self.imaginario)}i"
 def main():
     calculadora = Calculadora()
 
